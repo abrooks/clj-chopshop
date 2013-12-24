9adfc44d231386ee98b5b2acdd33732e87f5dc14
Aaron Brooks <aaron@brooks1.net>
2013-12-24 00:54:05 -0500

    Include Instaparse and create very toy Clojure-ish grammar.
    Also bumped Clojure version from 1.5.0-master-SNAPSHOT to 1.5.1.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject clj-chopshop "1.0.0-SNAPSHOT"
   :description "Programmatically transform Clojure source code"
-  :dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [instaparse "1.2.13"]]
   :dev-dependencies [[swank-clojure "1.4.0"]]
   :plugins [[swank-clojure "1.4.0"]])
--- src/clj_chopshop/core.clj
+++ src/clj_chopshop/core.clj
@@ -1,1 +1,24 @@
-(ns clj-chopshop.core)
+(ns clj-chopshop.core
+  (:require [instaparse.core :as insta]))
+
+(def clj-parse
+  (insta/parser "
+    CljSrc = SrcEnt*
+    SrcEnt = Readable | ReadIgnored
+    ReadIgnored = Comment | Comma | Whitespace
+    Comma = ','
+    Comment = ShebangComment | ReaderRemove | SemiComment
+    ShebangComment = '#!' #'.*'
+    SemiComment = ';' #'.*'
+    Whitespace = #'\\s+'
+    ReaderRemove = '#_' Whitespace* Readable
+    Readable = List | Map | Symbol (* Set ReaderPragma Vector ... *)
+    Symbol =  #'[A-Za-z0-9_]+' (* TODO: Not this *)
+    List = '(' SrcEnt* ')'
+    Map = '{' MapPairs* '}'
+    MapPairs = ReadIgnored? Readable ReadIgnored? Readable ReadIgnored?
+"))
+
+(comment
+  (clojure.pprint/pprint (clj-parse "#!/bin/bash\n(reduce {(;foo bar\n){}zip zap,herp derp})"))
+  )
