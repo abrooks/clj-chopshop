a6de9d61c1a11607196906e0655e4fc48d511000
Aaron Brooks <aaron@brooks1.net>
2013-12-26 14:37:39 -0500

    Add jclaggett's n01se/seqex library and example implementation.
    seqex is a good match for querying the parse tree. 'toplevel-se is
    equivalent to 'print-toplevels.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject clj-chopshop "1.0.0-SNAPSHOT"
   :description "Programmatically transform Clojure source code"
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [n01se/seqex "2.0.0"]
                  [instaparse "1.2.13"]]
   :dev-dependencies [[swank-clojure "1.4.0"]]
   :plugins [[swank-clojure "1.4.0"]])
--- src/clj_chopshop/core.clj
+++ src/clj_chopshop/core.clj
@@ -1,5 +1,6 @@
 (ns clj-chopshop.core
   (:require [instaparse.core :as insta]
+            [n01se.seqex :as se]
             [clojure.walk :as walk]))
 
 (def clj-parse
@@ -93,10 +94,33 @@
                            (rest s))))
         (println (reassemble s))))))
 
+(defn pnode [keyword & seqex]
+  (se/subex (apply se/cat keyword seqex)))
+
+(def comment-se
+  (pnode :SemiComment (se/cap se/n* rest)))
+
+(def sym-se
+  (pnode :Symbol (se/cap se/n1 first)))
+
+(def list-se
+  (pnode :List se/n*))
+
+(def toplevel-se
+  (se/rep* comment-se
+           (se/recap (pnode :List (se/rep 2 (se/cat se/n* sym-se)) se/n*)
+                     list)
+           se/n1))
+
 (comment
   (def t "#!/bin/bash\n(reduce \"foo\\n\" 123 12.34 \\c ^:foo ^ [1 2 3] #{a b}\\_ {(;foo bar\n){}zip zap,herp derp.ferp/derpy.derp} #())")
   (clojure.pprint/pprint (insta/transform {:String str} (clj-parse t)))
-  (time (chunk-parse clj-parse {:String #(vector :String (apply str %&))} (slurp "../clojure/src/clj/clojure/core.clj")))
+  (time (def y (chunk-parse clj-parse
+                            {:String #(vector :String (apply str %&))}
+                            (slurp "../clojure/src/clj/clojure/core.clj"))))
+  (time (def y (chunk-parse clj-parse
+                            {:String #(vector :String (apply str %&))}
+                            (slurp "../lein-voom/src/leiningen/voom.clj"))))
   (walk/postwalk #(if (coll? %)
                     (remove (fn [x]
                               (and (coll? x)
@@ -104,4 +128,6 @@
                             %)
                     %)
                  (clj-parse t))
+  (se/parse (se/rep* (se/cap (se/subex (se/cat :SemiComment se/n*))) se/n1) y)
+  (clojure.pprint/pprint (se/parse toplevel-se y))
   )
